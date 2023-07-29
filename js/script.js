ac13b5959fb6a375292126164367efa846104f77
Vinita Larokar <139438844+Vinitalarokar@users.noreply.github.com>
2023-07-29 17:17:23 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,236 @@
+
+/*home js*/
+
+const blogSection = document.querySelector('.blogs-section');
+
+db.collection("blogs").get().then((blogs) => {
+    blogs.forEach(blog => {
+        if(blog.id != decodeURI(location.pathname.split("/").pop())){
+            createBlog(blog);
+        }
+    })
+})
+
+
+const createBlog = (blog) => {
+    let data = blog.data();
+    blogSection.innerHTML += `
+    <div class="blog-card">
+        <img src="${data.bannerImage}" class="blog-image" alt="">
+        <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
+        <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
+        <a href="/${blog.id}" class="btn dark">read</a>
+    </div>
+    `;
+}
+
+
+/*editor js*/
+
+const blogTitleField = document.querySelector('.title');
+const articleFeild = document.querySelector('.article');
+
+// banner
+const bannerImage = document.querySelector('#banner-upload');
+const banner = document.querySelector(".banner");
+let bannerPath;
+
+const publishBtn = document.querySelector('.publish-btn');
+const uploadInput = document.querySelector('#image-upload');
+
+bannerImage.addEventListener('change', () => {
+    uploadImage(bannerImage, "banner");
+})
+
+uploadInput.addEventListener('change', () => {
+    uploadImage(uploadInput, "image");
+})
+
+const uploadImage = (uploadFile, uploadType) => {
+    const [file] = uploadFile.files;
+    if(file && file.type.includes("image")){
+        const formdata = new FormData();
+        formdata.append('image', file);
+
+        fetch('/upload', {
+            method: 'post',
+            body: formdata
+        }).then(res => res.json())
+        .then(data => {
+            if(uploadType == "image"){
+                addImage(data, file.name);
+            } else{
+                bannerPath = `${location.origin}/${data}`;
+                banner.style.backgroundImage = `url("${bannerPath}")`;
+            }
+        })
+    } else{
+        alert("upload Image only");
+    }
+}
+
+const addImage = (imagepath, alt) => {
+    let curPos = articleFeild.selectionStart;
+    let textToInsert = `\r![${alt}](${imagepath})\r`;
+    articleFeild.value = articleFeild.value.slice(0, curPos) + textToInsert + articleFeild.value.slice(curPos);
+}
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+publishBtn.addEventListener('click', () => {
+    if(articleFeild.value.length && blogTitleField.value.length){
+        // generating id
+        let letters = 'abcdefghijklmnopqrstuvwxyz';
+        let blogTitle = blogTitleField.value.split(" ").join("-");
+        let id = '';
+        for(let i = 0; i < 4; i++){
+            id += letters[Math.floor(Math.random() * letters.length)];
+        }
+
+        // setting up docName
+        let docName = `${blogTitle}-${id}`;
+        let date = new Date(); // for published at info
+
+        //access firstore with db variable;
+        db.collection("blogs").doc(docName).set({
+            title: blogTitleField.value,
+            article: articleFeild.value,
+            bannerImage: bannerPath,
+            publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+        })
+        .then(() => {
+            location.href = `/${docName}`;
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }
+})
+
+/*firebase js*/
+
+let firebaseConfig = {
+    // Enter your firebase credentials
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+
+/*blog js*/
+
+let blogId = decodeURI(location.pathname.split("/").pop());
+
+let docRef = db.collection("blogs").doc(blogId);
+
+docRef.get().then((doc) => {
+    if(doc.exists){
+        setupBlog(doc.data());
+    } else{
+        location.replace("/");
+    }
+})
+
+const setupBlog = (data) => {
+    const banner = document.querySelector('.banner');
+    const blogTitle = document.querySelector('.title');
+    const titleTag = document.querySelector('title');
+    const publish = document.querySelector('.published');
+    
+    banner.style.backgroundImage = `url(${data.bannerImage})`;
+
+    titleTag.innerHTML += blogTitle.innerHTML = data.title;
+    publish.innerHTML += data.publishedAt;
+
+    const article = document.querySelector('.article');
+    addArticle(article, data.article);
+}
+
+const addArticle = (ele, data) => {
+    data = data.split("\n").filter(item => item.length);
+    // console.log(data);
+
+    data.forEach(item => {
+        // check for heading
+        if(item[0] == '#'){
+            let hCount = 0;
+            let i = 0;
+            while(item[i] == '#'){
+                hCount++;
+                i++;
+            }
+            let tag = `h${hCount}`;
+            ele.innerHTML += `<${tag}>${item.slice(hCount, item.length)}</${tag}>`
+        } 
+        //checking for image format
+        else if(item[0] == "!" && item[1] == "["){
+            let seperator;
+
+            for(let i = 0; i <= item.length; i++){
+                if(item[i] == "]" && item[i + 1] == "(" && item[item.length - 1] == ")"){
+                    seperator = i;
+                }
+            }
+
+            let alt = item.slice(2, seperator);
+            let src = item.slice(seperator + 2, item.length - 1);
+            ele.innerHTML += `
+            <img src="${src}" alt="${alt}" class="article-image">
+            `;
+        }
+
+        else{
+            ele.innerHTML += `<p>${item}</p>`;
+        }
+    })
+}
+
+/* for other blog pages food, nature,music, technology */
+
+
+ function myFun(){
+    let searchval = document.getElementById("inp").value.toUpperCase();
+    console.log(searchval);
+    let table = document.getElementsByTagName("table");
+    let tr = document.getElementsByTagName("tr");
+
+    let front = document.querySelector(".mainPage");
+
+    for(let i=0; i<tr.length; i++){
+        let td = tr[i].getElementsByTagName("td")[0];
+        if(td){
+            let text = td.innerText;
+            if(text.toUpperCase().indexOf(searchval) > -1){
+                tr[i].style.display=""
+                front.style.display="none"
+            }else{
+                tr[i].style.display="none"
+                 
+
+
+            }
+        }
+    }
+
+    if(searchval == ""){
+        front.style.display="flex"
+
+    }
+ }
+
+//  IMAGE SLIDER
+
+let cust = document.querySelectorAll(".customer");
+let count = 0;
+console.log(cust);
+
+cust.forEach(function(review, index){
+    review.style.left=`${index * 100}%`
+    console.log(review);
+    console.log(index);
+
+})
+
+
+/* Login Form */
+
